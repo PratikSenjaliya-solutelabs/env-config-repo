b9858bbf3e4e85c9914f48d6414a2ea203068141
PRATIK SENJALIYA <pratik.s@solutelabs.com>
2023-02-09 12:20:49 +0530

    Temp commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import {SeeAllInsightsCard } from "@stylumia/core-ui";
 
 function App() {
   console.log("Window", window.sailConfig);
   return (
     <div className="App">
       <p>{window.envConfig.title}</p>
+
+      <SeeAllInsightsCard
+  className=" "
+  insights_numbers={13}
+  onClick={() => {}}
+  onClickSeeAll={function noRefCheck() {}}
+/>
     </div>
   );
 }
