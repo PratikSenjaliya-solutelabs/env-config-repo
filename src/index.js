9a0ade7ea5d1d81a274525bf4650732b6e2b3547
PRATIK SENJALIYA <pratik.s@solutelabs.com>
2023-02-16 14:12:27 +0530

    Page component added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "@stylumia/core-ui/dist/index.css"
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "@stylumia/core-ui/dist/index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {ToastContainer} from "@stylumia/core-ui"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ToastContainer/>
+      <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
